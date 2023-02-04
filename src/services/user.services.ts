19ea709c909710b366109ff21cbf72e8ce9e81e1
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-02-04 15:46:12 -0500

    format code

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,28 +1,26 @@
+import { IData } from "../interfaces/app.interface";
+import UserInstance from "../models/user.model";
+import { IUser } from "../interfaces/user.interfaces";
 
-import { IData } from '../interfaces/app.interface';
-import UserInstance from '../models/user.model';
-import { IUser } from '../interfaces/user.interfaces';
-
-
-
-async function getOne<T>(id : string): Promise<IData<T>> {
-   try {
-      const user =  await UserInstance.findByPk(id)
-      return {data : user?.toJSON() as T, error : false }
-   } catch (error : any) {
-       return {error : true , message : error.message}
-   }   
+async function getOne<T>(id: string): Promise<IData<T>> {
+  try {
+    const user = await UserInstance.findByPk(id);
+    return { data: user?.toJSON() as T, error: false };
+  } catch (error: any) {
+    return { error: true, message: error.message };
+  }
 }
 
-async function create<T>(params: IUser) : Promise<IData<T>> {
-   try {
-      const userCreate = await UserInstance.create(params)
-      return {data : userCreate as T, error : false}
-   } catch (error : any) {
-      return {error : true , message : error.message}
-   }
+async function create<T>(params: IUser): Promise<IData<T>> {
+  try {
+    const userCreate = await UserInstance.create(params);
+    return { data: userCreate as T, error: false };
+  } catch (error: any) {
+    return { error: true, message: error.message };
+  }
 }
+
 export default {
-   getOne,
-   create
-}
+  getOne,
+  create,
+};
