ceaa106c3d39e646bc4c8c63391922c6b0b688ce
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-02-06 21:35:48 -0500

    changes joi

--- src/hooks/validator.hook.ts
+++ src/hooks/validator.hook.ts
@@ -2,26 +2,20 @@ import { NextFunction, Response, Request } from "express";
 import * as joi from "joi";
 
 export const userScheme = joi.object({
-  id: joi.string().uuid().optional(),
-  name: joi.string().required(),
-  lastName: joi.string().optional(),
-  identificacion: joi.number().required().allow(""),
-  password: joi
-    .alternatives()
-    .conditional("identificacion", [
-      {
-        is: joi.any().valid(""),
-        then: joi.string().optional().allow(""),
-        otherwise: joi.string().required(),
-      },
-    ]),
+  id             : joi.string().uuid().optional(),
+  name           : joi.string().required(),
+  lastName       : joi.string().optional(),
+  identificacion : joi.number().required(),
+  password       : joi.string().required(),
 });
 
 export const validateSchema = (schema: joi.ObjectSchema) => {
   return (req: Request, res: Response, Next: NextFunction) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      return res.status(400).json({ message: error.message });
+      return res
+        .status(400)
+        .json({ title: "INVALID DATA", message: error.message });
     } else {
       Next();
     }
